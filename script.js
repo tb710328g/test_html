5031d97b88e2e47fd59584cd1749498bf26a5f4a
gg3be0 <tb710328g@gmail.com>
2025-02-17 18:26:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("hello.js")
+
+// 變數宣告 var const let 
+
+//let name ="qqqwwwwe"
+//let name1 ="中文測試"
+//console.log(name)
+//console.log(name+name1)
+
+
+// prompt() 可輸入()內的回答當成字串儲存    alert()警告視窗
+//fruit=prompt("喜歡的水果?")
+//console.log(fruit)
+//alert("感謝回答")
+
+//將輸入的變成數字  parseInt(prompt())
+
+//let food=parseInt(prompt("食物多少錢?"))
+//let tipPer=parseInt(prompt("小費有幾%"))/100
+//console.log(food)
+//console.log(tipPer)
+//let foodtip=food*tipPer
+//let total=food+foodtip
+//console.log(foodtip)
+//console.log("總金額是 " + total)
+//var i=1 
+//var replay
+//do{
+ 
+//console.log('第' + i + '隻綿羊')
+//i++
+//replay = prompt('輸入1繼續 輸入0結束')
+//}while(replay==1)
